fe3d510de0760b91f3920d5c255f91fcedf604a5
nikedinokodi <nikor.50@hotmail.com>
2023-05-06 00:48:59 +0300

    tet

--- QAT/App.js
+++ QAT/App.js
@@ -1,10 +1,20 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, View, TextInput, Button, Text } from 'react-native';
+import { StyleSheet, View, TextInput, Button, Text, TouchableOpacity } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 const Tab = createBottomTabNavigator();
+
+function ProjectViewScreen({ route }) {
+  const { projectName } = route.params;
+  return (
+    <View style={styles.container}>
+      <Text>{projectName}</Text>
+      {/* Add more details about the project here */}
+    </View>
+  );
+}
 
 export default function App() {
   const [name, setName] = useState('');
@@ -36,6 +46,17 @@ export default function App() {
       console.log('Error saving data:', error);
     }
   };
+  
+  const deleteData = async () => {
+    try {
+      await AsyncStorage.removeItem('@MySuperStore:questions');
+      console.log('Data deleted successfully');
+      setQuestions([]);
+    } catch (error) {
+      console.log('Error deleting data:', error);
+    }
+  };
+  
 
   return (
     <NavigationContainer>
@@ -43,10 +64,17 @@ export default function App() {
         <Tab.Screen name="Home" options={{ title: 'Home' }}>
           {() => (
             <View style={styles.container}>
-              <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}>
+              <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+                {questions.map((question, index) => (
+                  <TouchableOpacity key={index} onPress={() => navigation.navigate('ProjectView', { projectName: question })}>
+                    <Text>{question}</Text>
+                  </TouchableOpacity>
+                ))}
+              </View>
+              <View style={styles.inputContainer}>
                 <TextInput
-                  style={{ height: 40, borderColor: 'gray', borderWidth: 1, width: 200 }}
-                  placeholder="Enter your name"
+                  style={styles.input}
+                  placeholder="Add new project"
                   onChangeText={text => setName(text)}
                   value={name}
                 />
@@ -54,10 +82,16 @@ export default function App() {
                   title="Submit"
                   onPress={saveData}
                 />
+                <Button
+                  title="Delete data"
+                  onPress={deleteData}
+                  color="red"
+                />
               </View>
             </View>
           )}
         </Tab.Screen>
+  
         <Tab.Screen name="Questions" options={{ title: 'Questions' }}>
           {() => (
             <View style={styles.container}>
@@ -76,10 +110,12 @@ export default function App() {
             </View>
           )}
         </Tab.Screen>
+        <Tab.Screen name="ProjectView" component={ProjectViewScreen} options={{ title: 'Project View' }} />
       </Tab.Navigator>
     </NavigationContainer>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -87,5 +123,21 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  inputContainer: {
+    position: 'absolute',
+    bottom: 20,
+    left: 0,
+    right: 0,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    width: 200,
+    marginRight: 10,
+    paddingHorizontal: 10,
   },
 });
